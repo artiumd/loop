19dbd3e78e9a13f508124ed86d435a7d74052377
Artium <artium89@gmail.com>
2024-02-08 23:22:36 +0200

    Fix dynamic version

--- src/loop/__init__.py
+++ src/loop/__init__.py
@@ -3,7 +3,7 @@ from importlib.metadata import version, PackageNotFoundError
 
 # From: https://setuptools-scm.readthedocs.io/en/latest/usage/#version-at-runtime
 try:
-    __version__ = version("package-name")
+    __version__ = version("loop-python")
 except PackageNotFoundError:
     pass  # package is not installed
 
